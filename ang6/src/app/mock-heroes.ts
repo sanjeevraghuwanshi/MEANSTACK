ca5bbcc8087829cfd19e999c58f88e3cb1bf7b9a
Sanjeev Raghuwanshi <sanjeevraghuwanshi@gmail.com>
2018-05-20 10:44:47 +0530

    new projects

---
+++ ang6/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Ram' },
+    { id: 14, name: 'Sam' },
+    { id: 15, name: 'Shyam' },
+    { id: 16, name: 'Hari' },
+    { id: 17, name: 'Nari' },
+    { id: 18, name: 'Darsh' },
+    { id: 19, name: 'Shell' },
+    { id: 20, name: 'Tell' }
+]
---
+++ angularjs-sample-webpack-es6-karma-master/src/index.js
@@ -0,0 +1,5 @@
+import angular from "angular"
+import dummyComponent from "./dummy.component"
+
+angular.module("app", [dummyComponent])
+    .controller()

96cd26af9e897e5f5f8eceba3b16a596de34d66d
Sanjeev <sanjeevraghuwanshi@gmail.com>
2019-10-24 00:16:56 +0530

    changes done

--- jumpstart/src/app/core/growler/growler.module.ts
+++ jumpstart/src/app/core/growler/growler.module.ts
@@ -2,12 +2,13 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GrowlerComponent } from './growler.component';
 import { EnsureModuleLoadedOnceGuard } from '../ensure-module-loaded-once.guard';
+import { GrowlerService } from './growler.service';
 
 @NgModule({
   imports: [CommonModule],
   exports: [GrowlerComponent],
   declarations: [GrowlerComponent],
-  providers: []
+  providers: [GrowlerService]
 })
 export class GrowlerModule extends EnsureModuleLoadedOnceGuard {
   constructor(@Optional() @SkipSelf() parentModule: GrowlerModule) {
